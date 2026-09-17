af602869c9a09740c92abbbe2420a2a373a74353
MrAnonymous <flo111vel@gmail.com>
2026-09-17 14:20:09 +0200

    Even more edits

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -207,7 +207,7 @@ vim.g.tex_conceal = "abdmg"
 vim.g.tex_superscripts = "[0-9a-zA-W.,:;+-<>/()=]"
 vim.g.tex_subscripts = "[0-9aehijklmnoprstuvx,+-/().]"
 
-vim.g.UltiSnipsExpandTrigger = '<C-K>'
+--vim.g.UltiSnipsExpandTrigger = '<Tab>'
 vim.g.UltiSnipsJumpForwardTrigger = '<Tab>'
 vim.g.UltiSnipsJumpBackwardTrigger = '<S-Tab>'
 
